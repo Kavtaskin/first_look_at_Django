1a63c8ef46f971e15c39daa51b96fec5fbb70c17
Skobachke <skobachke@gmail.com>
2017-03-04 00:26:05 +0300

    New state

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@vix)zhuuqvfr3@_7g%lgfrk6r7n7-8=yxkr1908!j&l=wcc=0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'skoba.pythonanywhere.com']
 
